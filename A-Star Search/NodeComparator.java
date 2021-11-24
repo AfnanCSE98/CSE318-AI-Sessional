1ccb06c88322348ccd716dc1f71eab377c5ed06d
AfnanCSE98 <sihatafnan15.9.1997@gmail.com>
2021-11-24 13:08:19 +0600

    started

---
+++ A-Star Search/NodeComparator.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+/**
+ *
+ * @author Afnan
+ */
+
+public class NodeComparator implements Comparator<Node>{
+    @Override
+    public int compare(Node a , Node b){
+        if(a.f_cost < b.f_cost){
+            return -1;
+        }
+        else if(a.f_cost > b.f_cost){
+            return 1;
+        }
+        else return 0;
+    }
+}
